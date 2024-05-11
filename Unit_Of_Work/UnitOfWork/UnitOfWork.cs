76d56a79af81881b9fb49b31362b10f0581aedde
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-05-11 13:36:58 +0300

    lkdsldk

--- Unit_Of_Work/UnitOfWork/UnitOfWork.cs
+++ Unit_Of_Work/UnitOfWork/UnitOfWork.cs
@@ -4,6 +4,11 @@ using Unit_Of_Work.Repository;
 
 namespace Unit_Of_Work.UnitOfWork
 {
+
+	// Unit Of Work : İşlemlerin yönetimi kolaylaştıran,  işlemleri birbirine bağlayan  ve bu işlemlerin bir birleri ile gruplanmasını sağlayan bir tasarım desenidir
+	// Bu tasarım deseniyle birlikte, UnitOfWork tüm repository katmanını kendi içerisinde toplar.  Ve bu repository katmanlarının içerisnden geçecek trafik ile ilgili bazı kurallar koyar. Örnek : Transactional işlem gibi.
+	// 
+
 	public class UnitOfWork : IDisposable
 	{
 
@@ -20,9 +25,13 @@ namespace Unit_Of_Work.UnitOfWork
 			{
 				using (var transaction =  _context.Database.BeginTransaction())
 				{
+
+					// kategori kaydet
+					// alt kategori kaydet
 					try
 					{
 						// hata yoksa ise, işlem veri tabanına kaydedilsin.
+
 						_context.SaveChanges();
 						transaction.Commit();
 					}
